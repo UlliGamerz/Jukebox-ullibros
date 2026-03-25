772674e99d7fcef527794d94d5afb55c4c359f2f
UlliGamerz <97797820+UlliGamerz@users.noreply.github.com>
2026-03-25 18:56:19 +0530

    Update2 config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "nexus.voidhosting.vip", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 6004,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "cocaine", 
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "de-01.strixnodes.com", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT || 2010,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "glace", 
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'false' || false
             },
             
